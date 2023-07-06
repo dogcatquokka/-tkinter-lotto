b37fcea62dcf956e9966dd389cb061ea78f039e9
developer1 <dlwodn9910@naver.com>
2023-07-06 12:31:19 +0900

    수정전 본

--- main.py
+++ main.py
@@ -2,11 +2,37 @@
 
 import random
 
+lotto_num = range(1,46)
+
+for i in range(5):
+    print(random.sample(lotto_num,6))
+#여기까지 코드 작성을 마쳤다면 커밋을 실시하자.
+#tkinter 버튼으로 로또번호 생성하기
+#이제 버튼을 누르면 로또번호 생성이 진행되도록 main.py 의 코드를 아래와 같이 수정하자.
+import tkinter
+import tkinter.font
+import random
+lotto_num = range(1,46)
+
+def buttonClick():
+    print(random.sample(lotto_num,6))
+    
+    
+window=tkinter.Tk()
+window.title("lotto")
+window.geometry("400x200")
+window.resizable(False, False)
+
+button = tkinter.Button(window, overrelief="solid",text="번호확인", width=15, command=buttonClick, repeatdelay=1000, repeatinterval=100)
+button.pack()
+
+    
+    
+    
+window.mainloop()
 
 
-for i in range(5) :
-    lotto = random.sample(range(1,46,5))
-    print(lotto)
+
 
 
 
